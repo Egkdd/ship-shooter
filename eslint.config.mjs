711c425680be8c27ae91202f9fcd9ebe1fc7e408
Egkdd <herrbreuer1987@gmail.com>
2025-11-18 13:03:42 +0200

    updated project configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,17 @@
-import js from "@eslint/js";
-import prettier from "eslint-plugin-prettier/recommended";
-import tseslint from "typescript-eslint";
-
-export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      prettier,
-    ],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    rules: {},
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-);
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+  },
+  ignorePatterns: ["dist"],
+};
